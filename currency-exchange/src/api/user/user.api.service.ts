9916dd81ba3a6a837b90bb7173bb0854052440e3
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2025-01-22 16:22:44 +0100

    feat: Hash password when saving user to the database

--- currency-exchange/src/api/user/user.api.service.ts
+++ currency-exchange/src/api/user/user.api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/models/entities/user/user.entity';
 import { UserService } from 'src/models/entities/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserDto } from './dto/user.dto';
@@ -16,7 +17,12 @@ export class UserApiService {
    * @throws {UserExistsError} If a user with the given email already exists.
    */
   public async create(userDto: CreateUserDto): Promise<UserDto> {
-    const createdUser = await this.userService.create(userDto);
+    const hashedPassword = await bcrypt.hash(userDto.password, 10);
+
+    const createdUser = await this.userService.create({
+      ...userDto,
+      password: hashedPassword,
+    });
 
     return this.mapUserToDto(createdUser);
   }
