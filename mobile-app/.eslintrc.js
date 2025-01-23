bfcbab259d872ff3466bb92e2879bac036bd7589
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2025-01-23 12:57:30 +0100

    feat: Add unused imports plugin

--- mobile-app/.eslintrc.js
+++ mobile-app/.eslintrc.js
@@ -1,5 +1,18 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
+  plugins: ['unused-imports'],
   extends: 'expo',
   ignorePatterns: ['/dist/*'],
+  rules: {
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
